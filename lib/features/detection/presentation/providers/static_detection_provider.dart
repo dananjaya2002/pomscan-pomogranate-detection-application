bd29f71ce75a8d59393e99e1f52d36543a20a637
Danidu kavishan <127879066+dananjaya2002@users.noreply.github.com>
2026-05-08 14:56:20 +0530

    feat: enhance static detection page with ripeness detection and UI improvements

---
+++ lib/features/detection/presentation/providers/static_detection_provider.dart
@@ -0,0 +1,32 @@
+/// Riverpod providers for static-image ripeness detection.
+library;
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/constants/app_constants.dart';
+import '../../data/datasources/model_datasource.dart';
+import '../../data/repositories/detection_repository_impl.dart';
+import '../../domain/usecases/run_detection_usecase.dart';
+
+final staticModelDataSourceProvider = Provider<ModelDataSource>((ref) {
+  return ModelDataSource(
+    modelAssetPath: AppConstants.staticDetectionModelAssetPath,
+  );
+});
+
+/// Initialises the static-image model (float32) on demand.
+final staticModelInitProvider = FutureProvider<void>((ref) async {
+  final model = ref.read(staticModelDataSourceProvider);
+  await model.initialise();
+  ref.onDispose(model.dispose);
+});
+
+final staticDetectionRepositoryProvider = Provider((ref) {
+  return DetectionRepositoryImpl(
+    modelDataSource: ref.watch(staticModelDataSourceProvider),
+  );
+});
+
+final staticRunDetectionUseCaseProvider = Provider((ref) {
+  return RunDetectionUseCase(ref.watch(staticDetectionRepositoryProvider));
+});
